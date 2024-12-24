7ae60b8f415d193aa439b46edfef4ea6d2decf63
derektekk <derektekk@gmail.com>
2024-12-24 11:33:41 +1100

    Added components and pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
