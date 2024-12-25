bd35efbf933e15ab6bf7294e065fce9fb9e498e8
derektekk <derektekk@gmail.com>
2024-12-25 13:01:20 +1100

    Added styling to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import '../styles/Navbar.css'
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <ul>
-        <li>
+    <div className='navbar-container'>
+      <ul className='navbar-list'>
+        <li className='navbar-item'>
           <a href="#home">Home</a>
         </li>
-        <li>
+        <li className='navbar-item'>
           <a href="#about">About</a>
         </li>
-        <li>
+        <li className='navbar-item'>
           <a href="#projects">Projects</a>
         </li>
-        <li>
+        <li className='navbar-item'>
           <a href="#contact">Contact</a>
         </li>
       </ul>
